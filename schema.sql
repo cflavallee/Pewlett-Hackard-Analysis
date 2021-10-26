7abd62b6104386f0bccd5d7748b7d625cc50bb41
Collin LaVallee <clavallee2@gmail.com>
2021-10-25 22:55:13 -0500

    Query

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-	     emp_no INT NOT NULL,
+	 emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -35,13 +35,13 @@ CREATE TABLE salaries (
   	PRIMARY KEY (emp_no)
 );
 
+
 CREATE TABLE titles (
-	emp_no INT NOT NULL,
-	title VARCHAR(40) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+  	emp_no INT NOT NULL,
+  	title VARCHAR(40) NOT NULL,
+  	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)  	
 );
 
 CREATE TABLE dept_emp (
@@ -51,7 +51,36 @@ CREATE TABLE dept_emp (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no, dept_no)
 );
 
-SELECT * FROM departments;
+SELECT * FROM dept_manager;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create a new table of retiring age employees
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+Select * from retirement_info;
