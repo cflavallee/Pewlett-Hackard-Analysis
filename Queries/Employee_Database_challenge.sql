9d0ecd4b5d90ec2b8cdfea02e771ff8735ec5b3d
Collin LaVallee <clavallee2@gmail.com>
2021-11-04 18:36:43 -0500

    Challenge Started

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,64 @@
+--Employees Retiring List w/ Titles
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    t.title,
+    t.from_date,
+    t.to_date
+INTO retiring_employee_title
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+
+select * from retiring_employee_title;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO retiring_unique_titles
+FROM retiring_employee_title
+ORDER BY emp_no, to_date DESC;
+
+select * from retiring_unique_titles;
+
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM retiring_unique_titles
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+SELECT * FROM retiring_titles;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   t.title
+INTO employee_mentorship
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01');
+
+SELECT COUNT(emp_no), title
+INTO mentorship_titles
+FROM employee_mentorship
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+SELECT * FROM employee_mentorship;
+
+select * from titles
+WHERE emp_no = 10762;
+	
