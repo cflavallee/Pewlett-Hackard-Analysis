a92e2727936ea3467a8a1bb62967b88362b6da8b
Collin LaVallee <clavallee2@gmail.com>
2021-11-05 18:44:05 -0500

    Challenge Updated

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -13,9 +13,6 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
 
-select * from retiring_employee_title;
-
-
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
@@ -25,7 +22,6 @@ INTO retiring_unique_titles
 FROM retiring_employee_title
 ORDER BY emp_no, to_date DESC;
 
-select * from retiring_unique_titles;
 
  -- Get a count of employees retiring by title.
 SELECT COUNT(emp_no), title
@@ -34,7 +30,7 @@ FROM retiring_unique_titles
 GROUP BY title
 ORDER BY COUNT(emp_no) DESC;
 
-SELECT * FROM retiring_titles;
+
 
 -- Get the employees eligible for a mentorship program.
 SELECT DISTINCT ON (e.emp_no) e.emp_no,
@@ -53,16 +49,61 @@ ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01');
 
+-- Number of mentorship-eligible employees by title.
 SELECT COUNT(emp_no), title
 INTO mentorship_titles
 FROM employee_mentorship
 GROUP BY title
 ORDER BY COUNT(emp_no) DESC;
 
-SELECT * FROM retiring_titles;
 
-select * from titles
-WHERE emp_no = 10762;
 
+-- Employees by Hire Date
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   t.title
+INTO employee_mentorship_hire_date
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.from_date BETWEEN '1995-01-01' AND '2001-12-31')
+	AND (de.to_date = '9999-01-01');
+
+
+-- Number of mentorship-eligible employees by hire date and title.
+SELECT COUNT(emp_no), title
+INTO recent_hire_date_mentorship
+FROM employee_mentorship_hire_date
+GROUP BY employee_mentorship_hire_date.title
+ORDER BY COUNT(emp_no) DESC;
+
+
+-- Get the salaries for retire-ready employees.
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   s.salary,
+	   t.to_date,
+	   t.title
+INTO employee_retiring_salaries
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+LEFT JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (t.to_date = '9999-01-01');
+
+SELECT SUM(salary), title
+FROM employee_retiring_salaries
+GROUP BY employee_retiring_salaries.title;
 
 	
+select * from salaries;
